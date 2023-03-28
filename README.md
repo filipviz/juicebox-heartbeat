ed39ace89a4c69c1da93f5dd06c1e52f20a37f27
Filip <f@filip.world>
2023-03-28 00:19:11 -0400

    Canonical

---
+++ README.md
@@ -0,0 +1,19 @@
+# juice-pay-discord
+
+Send Discord Webhook notifications when any [Juicebox project](https://juicebox.money) receives a payment.
+
+## How to Run
+
+1. Create a .env file:
+
+```bash
+cp .example.env .env
+```
+
+2. Fill out your .env file.
+
+3. Install [node.js](https://nodejs.org/) (>=18.0.0), and run with:
+
+```bash
+node .
+```
---
+++ main.js
@@ -0,0 +1,91 @@
+import { schedule } from "node-cron";
+import dotenv from "dotenv";
+dotenv.config();
+
+const discord_webhook = process.env.DISCORD_WEBHOOK;
+const juicebox_subgraph = process.env.JUICEBOX_SUBGRAPH;
+
+// Run every minute
+schedule("* * * * *", () =>
+  main()
+    .then(() => console.log(`Successful run`))
+    .catch((e) => console.error(e))
+);
+
+// v2 payments within last minute
+const query = `{
+  payEvents(where:{timestamp_gt: ${
+    Math.floor(Date.now() / 1000) - 60
+  }, pv: "2"}){
+    project {
+      metadataUri
+    }
+    amount
+    projectId
+    beneficiary
+    txHash
+  }
+}`;
+
+async function main() {
+  fetch(juicebox_subgraph, {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then(async (json) => {
+      for (const payEvent of json.data.payEvents) {
+        console.log(`Notifying ${payEvent.txHash}`);
+        const metadata = await fetch(
+          `https://ipfs.io/ipfs/${payEvent.project.metadataUri}`
+        ).then((res) => res.json());
+        const project_name = metadata.name
+          ? metadata.name
+          : `project ${payEvent.projectId}`;
+        const ens = await fetch(
+          `https://api.ensideas.com/ens/resolve/${payEvent.beneficiary}`
+        ).then((res) => res.json());
+        const beneficiary = ens.name ? ens.name : payEvent.beneficiary;
+
+        // Post
+        fetch(discord_webhook, {
+          headers: { "Content-Type": "application/json" },
+          method: "POST",
+          body: JSON.stringify({
+            embeds: [
+              {
+                title: `Payment to ${project_name}`,
+                url: `https://etherscan.io/tx/${payEvent.txHash}`,
+                color: 9539981,
+                fields: [
+                  {
+                    name: `Amount`,
+                    value: `${payEvent.amount / 1e18} ETH`,
+                    inline: true,
+                  },
+                  {
+                    name: `Beneficiary`,
+                    value: `[${beneficiary}](https://etherscan.io/address/${payEvent.beneficiary})`,
+                    inline: true,
+                  },
+                  {
+                    name: `Project`,
+                    value: `[${project_name}](https://juicebox.money/v2/p/${payEvent.projectId})`,
+                    inline: true,
+                  },
+                ],
+              },
+            ],
+          }),
+          thumbnail: {
+            url: metadata.logoUri
+              ? `https://ipfs.io/ipfs/${metadata.logoUri.substring(
+                  metadata.logoUri.lastIndexOf("/") + 1
+                )}`
+              : undefined,
+          },
+        });
+      }
+    });
+}
