037b15a9c24c1caeeb5fcd1c01637e1d6abf2455
Filip <f@filip.world>
2023-06-28 09:21:34 -0400

    Use timestamps written to file

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
+import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
 // Constants
 const discord_webhook = process.env.DISCORD_WEBHOOK;
 const juicebox_subgraph = process.env.JUICEBOX_SUBGRAPH;
-const ONE_MINUTE_AGO = Math.floor(Date.now() / 1000) - 60;
 
-// For testing
-// const ONE_DAY_AGO = Math.floor(Date.now() / 1000) - 86400;
+if (!fs.existsSync("recent-runs.json")) {
+  fs.writeFileSync(
+    "recent-runs.json",
+    JSON.stringify({
+      lastPayEventTime: Math.floor(Date.now() / 1000),
+      lastProjectCreateEventTime: Math.floor(Date.now() / 1000),
+    }),
+    { encoding: "utf8" }
+  );
+}
+
+let { lastPayEventTime, lastProjectCreateEventTime } = JSON.parse(
+  fs.readFileSync("recent-runs.json")
+);
 
 const payEventsQuery = `{
-  payEvents(where:{timestamp_gt: ${ONE_MINUTE_AGO}}){
+  payEvents(where:{timestamp_gt: ${lastPayEventTime}}){
     project {
       handle
       metadataUri
@@ -20,11 +32,12 @@ const payEventsQuery = `{
     beneficiary
     txHash
     pv
+    timestamp
   }
 }`;
 
 const projectCreateEventsQuery = `{
-  projectCreateEvents(where:{timestamp_gt: ${ONE_MINUTE_AGO}}){
+  projectCreateEvents(where:{timestamp_gt: ${lastProjectCreateEventTime}}){
     project{
       handle
       metadataUri
@@ -33,6 +46,7 @@ const projectCreateEventsQuery = `{
     projectId
     txHash
     pv
+    timestamp
   }
 }`;
 
@@ -71,102 +85,132 @@ async function postToDiscordWebhook(title, url, fields, thumbnail) {
         },
       ],
     }),
-  })
+  });
 }
 
 async function handlePayEvents() {
-  querySubgraph(payEventsQuery).then(async (json) => {
-    for (const payEvent of json.data.payEvents) {
-      const [metadata, beneficiary] = await Promise.all([
-        resolveMetadata(payEvent.project.metadataUri),
-        resolveEns(payEvent.beneficiary),
-      ]);
-      const project_name = metadata.name
-        ? metadata.name
-        : `v${payEvent.pv} project ${payEvent.projectId}`;
+  return new Promise((resolve, reject) => {
+    querySubgraph(payEventsQuery).then(async (json) => {
+      for (const payEvent of json.data.payEvents) {
+        const [metadata, beneficiary] = await Promise.all([
+          resolveMetadata(payEvent.project.metadataUri),
+          resolveEns(payEvent.beneficiary),
+        ]);
+        const project_name = metadata.name
+          ? metadata.name
+          : `v${payEvent.pv} project ${payEvent.projectId}`;
 
-      postToDiscordWebhook(
-        `Payment to ${project_name}`,
-        `https://juicebox.money/${
-          payEvent.pv === "2"
-            ? "v2/p/" + payEvent.projectId
-            : "p/" + payEvent.project.handle
-        }`,
-        [
+        postToDiscordWebhook(
+          `Payment to ${project_name}`,
+          `https://juicebox.money/${
+            payEvent.pv === "2"
+              ? "v2/p/" + payEvent.projectId
+              : "p/" + payEvent.project.handle
+          }`,
+          [
+            {
+              name: `Amount`,
+              value: `${payEvent.amount / 1e18} ETH`,
+              inline: true,
+            },
+            {
+              name: `Beneficiary`,
+              value: `[${beneficiary}](https://juicebox.money/account/${payEvent.beneficiary})`,
+              inline: true,
+            },
+            {
+              name: `Transaction`,
+              value: `[Etherscan](https://etherscan.io/tx/${payEvent.txHash})`,
+              inline: true,
+            },
+          ],
           {
-            name: `Amount`,
-            value: `${payEvent.amount / 1e18} ETH`,
-            inline: true,
-          },
-          {
-            name: `Beneficiary`,
-            value: `[${beneficiary}](https://juicebox.money/account/${payEvent.beneficiary})`,
-            inline: true,
-          },
-          {
-            name: `Transaction`,
-            value: `[Etherscan](https://etherscan.io/tx/${payEvent.txHash})`,
-            inline: true,
-          },
-        ],
-        {
-          url: metadata.logoUri
-            ? `https://ipfs.io/ipfs/${metadata.logoUri.substring(
-                metadata.logoUri.lastIndexOf("/") + 1
-              )}`
-            : undefined,
-        }
-      );
-    }
+            url: metadata.logoUri
+              ? `https://ipfs.io/ipfs/${metadata.logoUri.substring(
+                  metadata.logoUri.lastIndexOf("/") + 1
+                )}`
+              : undefined,
+          }
+        )
+          .then(() => {
+            if (payEvent.timestamp > lastPayEventTime)
+              lastPayEventTime = payEvent.timestamp;
+          })
+          .catch((e) => {
+            fs.appendFileSync("errors.txt", e, { encoding: "utf8" });
+            reject(e);
+          });
+      }
+
+      resolve();
+    });
   });
 }
 
 async function handleCreateEvents() {
-  querySubgraph(projectCreateEventsQuery).then(async (json) => {
-    for (const projectCreateEvent of json.data.projectCreateEvents) {
-      const [metadata, from] = await Promise.all([
-        resolveMetadata(projectCreateEvent.project.metadataUri),
-        resolveEns(projectCreateEvent.from),
-      ]);
-      const project_name = metadata.name
-        ? metadata.name
-        : `v${projectCreateEvent.pv} project ${projectCreateEvent.projectId}`;
+  return new Promise((resolve, reject) => {
+    querySubgraph(projectCreateEventsQuery).then(async (json) => {
+      for (const projectCreateEvent of json.data.projectCreateEvents) {
+        const [metadata, from] = await Promise.all([
+          resolveMetadata(projectCreateEvent.project.metadataUri),
+          resolveEns(projectCreateEvent.from),
+        ]);
+        const project_name = metadata.name
+          ? metadata.name
+          : `v${projectCreateEvent.pv} project ${projectCreateEvent.projectId}`;
 
-      postToDiscordWebhook(
-        `New Project: ${project_name}`,
-        `https://juicebox.money/${
-          projectCreateEvent.pv === "2"
-            ? "v2/p/" + projectCreateEvent.projectId
-            : "p/" + projectCreateEvent.project.handle
-        }`,
-        [
+        postToDiscordWebhook(
+          `New Project: ${project_name}`,
+          `https://juicebox.money/${
+            projectCreateEvent.pv === "2"
+              ? "v2/p/" + projectCreateEvent.projectId
+              : "p/" + projectCreateEvent.project.handle
+          }`,
+          [
+            {
+              name: `Creator`,
+              value: `[${from}](https://juicebox.money/account/${projectCreateEvent.from})`,
+              inline: true,
+            },
+            {
+              name: `Transaction`,
+              value: `[Etherscan](https://etherscan.io/tx/${projectCreateEvent.txHash})`,
+              inline: true,
+            },
+            {
+              name: `Description`,
+              value: metadata.description,
+              inline: false,
+            },
+          ],
           {
-            name: `Creator`,
-            value: `[${from}](https://juicebox.money/account/${projectCreateEvent.from})`,
-            inline: true,
-          },
-          {
-            name: `Transaction`,
-            value: `[Etherscan](https://etherscan.io/tx/${projectCreateEvent.txHash})`,
-            inline: true,
-          },
-          {
-            name: `Description`,
-            value: metadata.description,
-            inline: false,
-          },
-        ],
-        {
-          url: metadata.logoUri
-            ? `https://ipfs.io/ipfs/${metadata.logoUri.substring(
-                metadata.logoUri.lastIndexOf("/") + 1
-              )}`
-            : undefined,
-        }
-      );
-    }
+            url: metadata.logoUri
+              ? `https://ipfs.io/ipfs/${metadata.logoUri.substring(
+                  metadata.logoUri.lastIndexOf("/") + 1
+                )}`
+              : undefined,
+          }
+        )
+          .then(() => {
+            if (projectCreateEvent.timestamp > lastProjectCreateEventTime)
+              lastProjectCreateEventTime = projectCreateEvent.timestamp;
+          })
+          .catch((e) => {
+            fs.appendFileSync("errors.txt", e, { encoding: "utf8" });
+            reject(e);
+          });
+      }
+      resolve();
+    });
   });
 }
 
-handlePayEvents();
-handleCreateEvents();
+await Promise.all([handlePayEvents(), handleCreateEvents()]);
+fs.writeFileSync(
+  "recent-runs.json",
+  JSON.stringify({
+    lastPayEventTime,
+    lastProjectCreateEventTime,
+  }),
+  { encoding: "utf8" }
+);
